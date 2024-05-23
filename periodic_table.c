d92c86277a5fd417a92eac73aba13d8dd70c0e50
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-05-23 18:11:56 -0500

    prepping for cont elements

--- periodic_table.c
+++ periodic_table.c
@@ -47,5 +47,37 @@ int main()
 
         printf("ENTER\n");
         scanf("%d", &Exit);
+
+        if(Exit==6)
+        {
+            printf("Are you sure, you want to close the periodic table? (Yes/No)\n");
+            printf("> Press 6 for Y\n");
+            printf("> Press 7 for No\n");
+
+            printf("ENTER\n");
+            scanf("%d", &Exit);
+
+            if(Exi==4)
+            {
+                printf("The periodic table has closed");
+            }
+            else if(Exi==5)
+            {
+                printf("Periodic table has not closed and  you can continue to learn more about elements\n\n");
+
+                printf("Enter the atomic number of the element to be searched : \n\n");
+                scanf("%d", &a);
+
+                if(a==1)
+                {
+                    printf("Name : Hydrogen\n");
+                    printf("Symbol : H");
+                    printf("Atomic Number : 1\n");
+                    printf("Electronic Configuration : 1s^1\n");
+                    printf("Discover By : Henry Cavendish\n");
+                    printf("Charge : +1\n");
+                }
+            }
+        }
     }
 }
